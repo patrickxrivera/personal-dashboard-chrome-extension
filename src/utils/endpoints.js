db901f65b73adbe3fdb3e4d1880ea240cf66ac8a
Patrick Rivera <pxr13@my.fsu.edu>
2018-06-06 19:26:45 -0400

    Edited env variables

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,5 +1,7 @@
 import PH_DEV_TOKEN from '../keys';
 
+const isNetlifyEnv = () => window.location.href.includes('.netflify.com');
+
 export const HN_TOP_STORIES = 'https://hacker-news.firebaseio.com/v0/topstories.json';
 export const HN_STORY = 'https://hacker-news.firebaseio.com/v0/item/';
 export const HN_COMMENTS_ROOT = 'https://news.ycombinator.com/item?id=';
@@ -8,6 +10,6 @@ export const PH_TOP_POSTS = 'https://api.producthunt.com/v1/me/feed';
 
 export const PH_CONFIG = {
   headers: {
-    Authorization: `Bearer ${PH_DEV_TOKEN}`
+    Authorization: `Bearer ${isNetlifyEnv() ? process.env.REACT_APP_PROD_PH_TOKEN : PH_DEV_TOKEN}`
   }
 };
